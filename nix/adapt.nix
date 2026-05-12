94c19632f5035b6f99ea2e69e84292e4f0ef7f3d
Victor Hugo Borja <vborja@apache.org>
2026-05-11 18:32:27 -0600

    adapt

--- nix/adapt.nix
+++ nix/adapt.nix
@@ -2,15 +2,10 @@ lens: cmap: smap: fmap: {
   get =
     t:
     let
-      e_s = cmap t;
+      s = cmap t;
+      a = lens.get s.right;
     in
-    if e_s ? right then
-      let
-        e_a = lens.get e_s.right;
-      in
-      if e_a ? right then fmap e_a.right else e_a
-    else
-      e_s;
+    if s ? right then if a ? right then fmap a.right else a else s;
 
   set = t: b: smap t (lens.set (cmap t).right b);
 }
